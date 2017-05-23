9210bcb862767eaf8f2e14241adc7abbfe8690c7
Martin <martinlauridsen_93@hotmail.com>
2017-05-23 16:17:39 +0200

    pictures downloads both in dayview and detailview

--- app/src/main/java/com/example/kenneth/examproject/Adapters/DayListAdapter.java
+++ app/src/main/java/com/example/kenneth/examproject/Adapters/DayListAdapter.java
@@ -1,6 +1,8 @@
 package com.example.kenneth.examproject.Adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v4.util.LruCache;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,10 +10,16 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+import com.android.volley.toolbox.Volley;
 import com.example.kenneth.examproject.Models.Event;
 import com.example.kenneth.examproject.R;
 
 import java.util.List;
+
+import static com.example.kenneth.examproject.R.id.parent;
 
 /**
  * Created by Kenneth on 20-04-2017.
@@ -22,10 +30,27 @@ public class DayListAdapter extends BaseAdapter {
     Context context;
     List<Event> events;
     Event event = null;
+    NetworkImageView imageView;
+    private ImageLoader mImageLoader;
+    private RequestQueue mRequestQueue;
+
 
     public DayListAdapter(Context c, List<Event> taskList){
         events = taskList;
         context = c;
+
+        mRequestQueue = Volley.newRequestQueue(c);
+        mImageLoader = new ImageLoader(mRequestQueue, new ImageLoader.ImageCache() {
+            private final LruCache<String, Bitmap> mCache = new LruCache<String, Bitmap>(10);
+
+            public void putBitmap(String url, Bitmap bitmap) {
+                mCache.put(url, bitmap);
+            }
+
+            public Bitmap getBitmap(String url) {
+                return mCache.get("url");
+            }
+        });
     }
     @Override
     public int getCount()
@@ -62,8 +87,8 @@ public class DayListAdapter extends BaseAdapter {
         event = events.get(position);
         if(event !=null) {
 
-            ImageView eventImageView = (ImageView) convertView.findViewById(R.id.eventIV);
-            eventImageView.setImageBitmap(event.getEventImage());
+            //ImageView eventImageView = (ImageView) convertView.findViewById(R.id.eventIV);
+            //eventImageView.setImageBitmap(event.getEventImage());
 
             TextView nameView = (TextView) convertView.findViewById(R.id.eventNameTV);
             nameView.setText(event.getName().toString());
@@ -73,6 +98,9 @@ public class DayListAdapter extends BaseAdapter {
 
             TextView descView = (TextView) convertView.findViewById(R.id.eventDescTV);
             String s = event.getDescrition().substring(0,100);
+
+            imageView = (NetworkImageView) convertView.findViewById(R.id.eventIV);
+            imageView.setImageUrl(event.getEventImage(), mImageLoader);
             descView.setText(s);
         }
         return convertView;
